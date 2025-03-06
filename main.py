953f53d9a9f81cd23e2731dc25f85ba162f76128
Jump-play <yusuf.bodi12@gmail.com>
2025-03-06 19:27:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import asyncio
-
+import pygame
 # Try to declare all your globals at once to facilitate compilation later.
 COUNT_DOWN = 3
 
 # Do init here
-# Load any assets right now to avoid lag at runtime or network errors.
+# Load any assets right now to avoid lag at runtimepygame.mixer.music.load('assets/assets_boss.mp3')
+pygame.mixer.music.set_volume(0.6)
+pygame.mixer.music.play(-1, 0.0)
+jump_fx = pygame.mixer.Sound('assets/jump.mp3')
+jump_fx.set_volume(0.5)
+death_fx = pygame.mixer.Sound('assets/death.mp3')
+death_fx.set_volume(0.5)or network errors.
 
 
 async def main():
